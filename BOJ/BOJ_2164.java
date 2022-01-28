6727e5ac85332145fea152d0461efddbca67f476
KangInPark <pkihome@naver.com>
2022-01-28 21:39:53 +0900

    최초 커밋: BOJ_2164번 문제풀이

---
+++ BOJ/BOJ_2164.java
@@ -0,0 +1,21 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class BOJ_2164 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Queue<Integer> q = new LinkedList<>();
+        int n = sc.nextInt();
+        for(int i = 1 ; i <= n; i++){
+            q.add(i);
+        }
+        while(q.size() > 1){
+            q.poll();
+            int tmp = q.poll();
+            q.add(tmp);
+        }
+        System.out.println(q.poll());
+    }
+
+}
