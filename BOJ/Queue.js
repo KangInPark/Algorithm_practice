d114ebcff3a84d16cf01d8c4c8b339428e4c1dd8
KangInPark <pkihome@naver.com>
2022-02-15 16:02:50 +0900

    최초커밋: js 문제풀이에서 사용할 큐 Class

---
+++ BOJ/Queue.js
@@ -0,0 +1,27 @@
+class Queue {
+    constructor(size) {
+        this.arr = Array.from({ length: size });
+        this.head = 0;
+        this.tail = 0;
+        this.size = size;
+        this.len = 0;
+    }
+    push(n) {
+        if (this.len === this.size) return -1;
+        this.arr[this.tail] = n;
+        this.tail = (this.tail + 1) % this.size;
+        this.len++;
+        return 1;
+    }
+    pop() {
+        if (this.empty()) return -1;
+        let ret = this.arr[this.head];
+        this.head = (this.head + 1) % this.size;
+        this.len--;
+        return ret;
+    }
+    empty() {
+        if (this.len == 0) return true;
+        else return false;
+    }
+}
