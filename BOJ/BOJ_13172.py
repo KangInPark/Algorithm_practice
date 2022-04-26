6ba910e37a401ff9a7ef1346f2567643aa35afbf
KangInPark <pkihome@naver.com>
2022-04-26 23:55:38 +0900

    수정: 리팩토링

--- BOJ/BOJ_13172.py
+++ BOJ/BOJ_13172.py
@@ -3,18 +3,19 @@ import sys
 
 def power(x, n):
     if n == 1:
-        return x % 1000000007
+        return x % r
     if n % 2 == 0:
         tmp = power(x, n // 2)
-        return (tmp * tmp) % 1000000007
+        return (tmp * tmp) % r
     else:
         tmp = power(x, n - 1)
-        return (x * tmp) % 1000000007
+        return (x * tmp) % r
 
 
 m = int(sys.stdin.readline())
 ret = 0
+r = 1000000007
 for _ in range(m):
     a, b = map(int, sys.stdin.readline().split(" "))
-    ret += b * power(a, 1000000007 - 2) % 1000000007
-print(ret % 1000000007)
+    ret += b * power(a, r - 2) % r
+print(ret % r)
